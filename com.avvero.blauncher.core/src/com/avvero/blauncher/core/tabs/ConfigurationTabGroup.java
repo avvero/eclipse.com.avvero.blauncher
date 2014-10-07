fca861b43e9edd0cd6c5fb6f83ebe081402381e6
Avvero <avvero@mail.ru>
2014-10-07 17:41:09 +0700

    Вынос core части https://www.eclipse.org/articles/Article-Launch-Framework/launch.html

---
+++ com.avvero.blauncher.core/src/com/avvero/blauncher/core/tabs/ConfigurationTabGroup.java
@@ -0,0 +1,23 @@
+package com.avvero.blauncher.core.tabs;
+
+import org.eclipse.debug.ui.AbstractLaunchConfigurationTabGroup;
+import org.eclipse.debug.ui.CommonTab;
+import org.eclipse.debug.ui.ILaunchConfigurationDialog;
+import org.eclipse.debug.ui.ILaunchConfigurationTab;
+
+/**
+ * Launch configuration tab groups
+ * @author belyaev-ay
+ *
+ */
+public class ConfigurationTabGroup extends AbstractLaunchConfigurationTabGroup {
+
+	@Override
+	public void createTabs(ILaunchConfigurationDialog dialog, String mode) {
+		setTabs(new ILaunchConfigurationTab[] { 
+				new SelectLouncherTab(),
+				new CommonTab()
+				});
+	}
+
+}
